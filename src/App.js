a2b2ec90642b785e85e57fae36506d0b2a5d06d5
shotaKvitsashvili <shotaKvitsashvili>
2021-03-25 23:17:08 +0400

    start.point

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
 
 function App() {
+
+  const [users, setusers] = useState([])
+  
+  useEffect(() => {
+
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+      .then(res => res.json())
+      .then(res => setusers(res));
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {
+       users.map((value, index)=> {
+        return <div key={index}>
+          <span>{value.name}</span>
+        </div>
+       })
+     }
     </div>
   );
 }
